88f94dfb4990c8bd7610464e74008d8a264eaf4f
dirty49374 <dirty49374@github.com>
2022-03-05 00:21:49 +0900

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode >,
   document.getElementById('root')
